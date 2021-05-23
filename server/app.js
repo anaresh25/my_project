cfc8d32e4691326e5d343364dac230d3b96f5c92
naresh-rao <naresh_m180281ca@nitc.ac.in>
2021-05-23 12:09:25 +0530

    50MB

--- server/app.js
+++ server/app.js
@@ -16,7 +16,9 @@ const bodyParser = require("body-parser");
 
 var path = require("path");
 
-app.use(express.json())
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb" }));
+// app.use(express.json())
 
 mongoose.connect(MONGOURI, {
   useNewUrlParser: true,
